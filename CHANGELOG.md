fc6cc2bb4e7a0455c92d234580b6e6618099dae3
Isa Ozler <isaozler@gmail.com>
2024-06-26 04:02:17 +0200

    refactored

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
 # Changelog
 
-**26 June 2024. Version 0.1.0 ~ 0.1.11**
+**26 June 2024. Version 0.1.0 ~ 0.1.12**
 - Initial release
--- mod.ts
+++ mod.ts
@@ -2,6 +2,13 @@
  * A module providing the data from isaozler.com.
  * @module
  *
+ * @example
+ * ```ts
+ * import * as data from '@isaozler/data/get'
+ *
+ * // data.all -> { sections: [ { sectionData: "see types" } ] }
+ * // data.siteSettings: TSiteSettings
+ * ```
  */
 
 export * from "./types/index.ts"
